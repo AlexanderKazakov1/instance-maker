9aaa309fc96f6b8c1d07929d9d6f8b2508777a36
Alexander Kazakov <gips73rus@mail.ru>
2023-03-09 17:20:53 +0400

    added terraform

---
+++ config.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.86.0"
+    }
+  }
+}
+
+provider "yandex" {
+  cloud_id                 = "b1gprcjj64afi0vdqjdv"
+  folder_id                = "b1gpjrnl8ltbu6on2c0t"
+  zone                     = "ru-central1-a"
+}
+
+variable "name" {
+  type = string
+  description = "Instance name"
+}
+
+variable "cores" {
+  type = string
+  default = 4
+  description = "CPU cores"
+}
+
+variable "memory" {
+  type = string
+  default = 4
+  description = "RAM memory"
+}
+
+variable "image_id" {
+  type = string
+  default = "fd8emvfmfoaordspe1jr"
+  description = "ID image from cloud"
+}
+
+resource "yandex_compute_instance" "instance" {
+  name        = var.name
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = var.cores
+    memory = var.memory
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet.id}"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "generated_network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.network.id}"
+  v4_cidr_blocks = ["10.128.0.31/24"]
+}
